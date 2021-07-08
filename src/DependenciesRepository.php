acb5f688cf873cdfd453a49e19374b4133cb31ab
Stephen Neal <stephen.neal14@gmail.com>
2021-07-08 15:04:56 -0400

    ADD sourcecode from hpa app

---
+++ src/DependenciesRepository.php
@@ -0,0 +1,92 @@
+<?php
+
+
+namespace Sfneal\Dependencies;
+
+
+use Illuminate\Support\Collection;
+use Sfneal\Helpers\Strings\StringHelpers;
+
+class DependenciesRepository
+{
+    /**
+     * @var bool Use composer dependencies instead of list of dependencies from the config
+     */
+    private $allComposerDependencies;
+
+    /**
+     * DependenciesRepository constructor.
+     *
+     * @param bool $allComposerDependencies
+     */
+    public function __construct(bool $allComposerDependencies = false)
+    {
+        $this->allComposerDependencies = $allComposerDependencies;
+    }
+
+    /**
+     * Retrieve a Collection of Dependencies with GitHub, Packagist verison & Travis CI build status URLs.
+     *
+     * @return Collection
+     */
+    public function get(): Collection
+    {
+        return $this->getDependencies()->map(function (string $type, string $dependency) {
+            return new DependenciesService($dependency, $type);
+        });
+    }
+
+    /**
+     * Retrieve a list of dependencies from the config file or by reading the composer.json 'requires' section.
+     *
+     * @return Collection
+     */
+    private function getDependencies(): Collection
+    {
+        if ($this->allComposerDependencies) {
+            return self::getComposerRequirements();
+        }
+        return self::getConfigDependencies() ?? self::getComposerRequirements();
+    }
+
+    /**
+     * Retrieve a list of dependencies set in the 'dependencies' config.
+     *
+     * @return Collection
+     */
+    private function getConfigDependencies(): Collection
+    {
+        // Convert array of dependency type keys & array of dependency values
+        // to a flat array of dependency keys and type values
+        return collect(config('dependencies.dependencies'))
+            ->mapWithKeys(function ($packages, $type) {
+                return collect($packages)
+                    ->mapWithKeys(function (string $package) use ($type) {
+                        return [$package => $type];
+                    });
+            });
+    }
+
+    /**
+     * Retrieve an array of composer packages that are required by the composer.json.
+     *
+     * @return Collection
+     */
+    private static function getComposerRequirements(): Collection
+    {
+        // Retrieve 'require' array from composer.json with only package names (the keys
+        return collect(array_keys(
+                json_decode(file_get_contents(base_path('composer.json')), true )['require'])
+            )
+
+            // Remove 'php' & php extensions from the packages array
+            ->filter(function (string $dep) {
+                return $dep != 'php' && ! (new StringHelpers($dep))->inString('ext');
+            })
+
+            // Map each dependencies to have a 'composer' value
+            ->mapWithKeys(function (string $dep) {
+                return [$dep => 'composer'];
+            });
+    }
+}
---
+++ src/DependenciesService.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Sfneal\Dependencies;
+
+class DependenciesService
+{
+    /**
+     * @var string Name of the sfneal composer dependency
+     */
+    public $package;
+
+    /**
+     * @var string Type of Dependency ('composer' or 'docker')
+     */
+    public $type;
+
+    /**
+     * DependenciesService constructor.
+     * @param string $package
+     * @param string $type
+     */
+    public function __construct(string $package, string $type = 'composer')
+    {
+        $this->package = $package;
+        $this->type = $type;
+    }
+
+    /**
+     * Retrieve a GitHub URL for a dependency.
+     *
+     * @return string
+     */
+    public function gitHub(): string
+    {
+        return self::url("github.com/{$this->packageName()}");
+    }
+
+    /**
+     * Retrieve a Travis CI build status SVG URL for a dependency.
+     *
+     * @param bool $svg
+     * @return string
+     */
+    public function travis(bool $svg = false): string
+    {
+        return self::url("travis-ci.com/{$this->packageName()}".($svg ? '.svg?branch=master' : ''));
+    }
+
+    /**
+     * Retrieve the Dependencies latest version.
+     *
+     * @param bool $svg
+     * @return string
+     */
+    public function version(bool $svg = false): string
+    {
+        return $this->type == 'composer' ? $this->packagist($svg) : $this->docker($svg);
+    }
+
+    /**
+     * Retrieve date of the last GitHub commit.
+     *
+     * @return string
+     */
+    public function lastCommit(): string
+    {
+        return self::imgShieldUrl("/github/last-commit/{$this->packageName()}");
+    }
+
+    /**
+     * Retrieve a Packagist versions SVG URL for a dependency.
+     *
+     * @param bool $svg
+     * @return string
+     */
+    private function packagist(bool $svg = false): string
+    {
+        if ($svg) {
+            return self::imgShieldUrl("/packagist/v/{$this->package}.svg");
+        } else {
+            return self::url("packagist.org/packages/{$this->package}");
+        }
+    }
+
+    /**
+     * Retrieve the latest Docker image tag for a dependency.
+     *
+     * @param bool $svg
+     * @return string
+     */
+    private function docker(bool $svg = false): string
+    {
+        if ($svg) {
+            return self::imgShieldUrl("/docker/v/{$this->package}.svg?sort=semver");
+        } else {
+            return self::url("hub.docker.com/r/{$this->package}");
+        }
+    }
+
+    /**
+     * Retrieve the correct name for a package.
+     *
+     * @return string
+     */
+    private function packageName(): string
+    {
+        if ($this->type == 'docker') {
+            return str_replace('stephenneal', 'sfneal', $this->package);
+        }
+
+        return $this->package;
+    }
+
+    /**
+     * Retrieve a image shield url.
+     *
+     * @param string $endpoint
+     * @return string
+     */
+    private static function imgShieldUrl(string $endpoint): string
+    {
+        return self::url('img.shields.io'.$endpoint);
+    }
+
+    /**
+     * Retrieve a secure url.
+     *
+     * @param string $uri
+     * @param bool $secure use https
+     * @return string
+     */
+    private static function url(string $uri, bool $secure = true): string
+    {
+        return 'http'.($secure ? 's' : '')."://{$uri}";
+    }
+}
