1dc4cfb5da26289efe7bed4d8a82fc75ac5ff700
Stephen Neal <stephen.neal14@gmail.com>
2021-07-08 19:59:08 -0400

    CUT `urlAssertions()` method

--- tests/Feature/DependencyServiceTest.php
+++ tests/Feature/DependencyServiceTest.php
@@ -12,11 +12,14 @@ use Sfneal\Dependencies\Tests\TestCase;
 class DependencyServiceTest extends TestCase
 {
     /**
-     * @param string $url
+     * @test
+     * @dataProvider packageProvider
+     * @param string $package
      * @throws GuzzleException
      */
-    public function urlAssertions(string $url)
+    public function github_url(string $package)
     {
+        $url = (new DependenciesService($package))->gitHub();
         $response = (new Client())->request('get', $url);
 
         $this->assertEquals(200, $response->getStatusCode());
@@ -28,24 +31,12 @@ class DependencyServiceTest extends TestCase
      * @param string $package
      * @throws GuzzleException
      */
-    public function github_url(string $package)
-    {
-        $url = (new DependenciesService($package))->gitHub();
-
-        $this->urlAssertions($url);
-    }
-
-    /**
-     * @test
-     * @dataProvider packageProvider
-     * @param string $package
-     * @throws GuzzleException
-     */
     public function travis_url(string $package)
     {
         $url = (new DependenciesService($package))->travis();
+        $response = (new Client())->request('get', $url);
 
-        $this->urlAssertions($url);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 
     /**
@@ -57,8 +48,9 @@ class DependencyServiceTest extends TestCase
     public function version_url(string $package)
     {
         $url = (new DependenciesService($package))->version();
+        $response = (new Client())->request('get', $url);
 
-        $this->urlAssertions($url);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 
     /**
@@ -70,8 +62,9 @@ class DependencyServiceTest extends TestCase
     public function travis_url_svg(string $package)
     {
         $url = (new DependenciesService($package))->travis(true);
+        $response = (new Client())->request('get', $url);
 
-        $this->urlAssertions($url);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 
     /**
@@ -83,8 +76,9 @@ class DependencyServiceTest extends TestCase
     public function version_url_svg(string $package)
     {
         $url = (new DependenciesService($package))->version(true);
+        $response = (new Client())->request('get', $url);
 
-        $this->urlAssertions($url);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 
     /**
@@ -96,7 +90,8 @@ class DependencyServiceTest extends TestCase
     public function last_commit_url(string $package)
     {
         $url = (new DependenciesService($package))->lastCommit();
+        $response = (new Client())->request('get', $url);
 
-        $this->urlAssertions($url);
+        $this->assertEquals(200, $response->getStatusCode());
     }
 }
