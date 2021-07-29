90218d36b77ba6ce91b52ed3ff8fa6de744da828
Stephen Neal <stephen.neal14@gmail.com>
2021-07-29 14:51:43 -0400

    ADD $sendRequest param to Url & Svg assertion methods so that repo & service tests can be speed up

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -82,116 +82,147 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         $this->assertSame($expected, $collection->count());
 
         $collection->each(function (DependenciesService $service) {
-            $this->assertTravisSvg($service->packageGitubName, $service->travis());
-            $this->assertVersionSvg($service->package, $service->version());
-            $this->assertLastCommitSvg($service->packageGitubName, $service->lastCommit());
-            $this->assertGithubUrl($service->packageGitubName, $service->gitHub());
-            $this->assertTravisUrl($service->packageGitubName, $service->travis());
-            $this->assertVersionUrl($service->package, $service->version());
+            $this->assertTravisSvg($service->packageGitubName, $service->travis(), false);
+            $this->assertVersionSvg($service->package, $service->version(), false);
+            $this->assertLastCommitSvg($service->packageGitubName, $service->lastCommit(), false);
+            $this->assertGithubUrl($service->packageGitubName, $service->gitHub(), false);
+            $this->assertTravisUrl($service->packageGitubName, $service->travis(), false);
+            $this->assertVersionUrl($service->package, $service->version(), false);
         });
     }
 
     /**
      * @param string $package
      * @param DependencySvg $generator
+     * @param bool $sendRequest
      */
-    public function assertTravisSvg(string $package, DependencySvg $generator)
+    public function assertTravisSvg(string $package, DependencySvg $generator, bool $sendRequest = true)
     {
         $url = $generator->svg();
-        $response = Http::get($url);
 
         $this->assertInstanceOf(DependencyUrl::class, $generator);
         $this->assertInstanceOf(DependencySvg::class, $generator);
         $this->assertStringContainsString($package, $url);
         $this->assertStringContainsString('travis-ci.com', $url);
         $this->assertStringContainsString('.svg?branch=master', $url);
-        $this->assertTrue($response->ok());
-        $this->assertStringContainsString('build', $response->body());
+
+        if ($sendRequest) {
+            $response = Http::get($url);
+
+            $this->assertTrue($response->ok());
+            $this->assertStringContainsString('build', $response->body());
+        }
     }
 
     /**
      * @param string $package
      * @param DependencySvg $generator
+     * @param bool $sendRequest
      */
-    public function assertVersionSvg(string $package, DependencySvg $generator)
+    public function assertVersionSvg(string $package, DependencySvg $generator, bool $sendRequest = true)
     {
         $url = $generator->svg();
-        $response = Http::get($url);
 
         $this->assertInstanceOf(DependencyUrl::class, $generator);
         $this->assertInstanceOf(DependencySvg::class, $generator);
         $this->assertStringContainsString($package, $url);
         $this->assertStringContainsString('img.shields.io/', $url);
-        $this->assertTrue($response->ok());
 
-        $inString = (new StringHelpers($response->body()));
-        $this->assertTrue(
-            $inString->inString('<title>packagist: v') || $inString->inString('version')
-        );
+        if ($sendRequest) {
+            $response = Http::get($url);
+
+            $this->assertTrue($response->ok());
+            $this->assertStringContainsString('build', $response->body());
+
+            $inString = (new StringHelpers($response->body()));
+            $this->assertTrue(
+                $inString->inString('<title>packagist: v') || $inString->inString('version')
+            );
+        }
     }
 
     /**
      * @param string $package
      * @param DependencySvg $generator
+     * @param bool $sendRequest
      */
-    public function assertLastCommitSvg(string $package, DependencySvg $generator)
+    public function assertLastCommitSvg(string $package, DependencySvg $generator, bool $sendRequest = true)
     {
         $url = $generator->svg();
-        $response = Http::get($url);
 
         $this->assertInstanceOf(DependencyUrl::class, $generator);
         $this->assertInstanceOf(DependencySvg::class, $generator);
         $this->assertStringContainsString($package, $url);
         $this->assertStringContainsString('img.shields.io/github/last-commit', $url);
-        $this->assertTrue($response->ok());
-        $this->assertStringContainsString('last commit', $response->body());
+
+        if ($sendRequest) {
+            $response = Http::get($url);
+
+            $this->assertTrue($response->ok());
+            $this->assertStringContainsString('last commit', $response->body());
+        }
     }
 
     /**
      * @param string $package
      * @param DependencyUrl $generator
+     * @param bool $sendRequest
      */
-    public function assertGithubUrl(string $package, DependencyUrl $generator)
+    public function assertGithubUrl(string $package, DependencyUrl $generator, bool $sendRequest = true)
     {
         $url = $generator->url();
-        $response = Http::get($url);
 
         $this->assertInstanceOf(DependencyUrl::class, $generator);
         $this->assertStringContainsString($package, $url);
         $this->assertStringContainsString('github.com', $url);
-        $this->assertTrue($response->ok());
+
+        if ($sendRequest) {
+            $response = Http::get($url);
+
+            $this->assertTrue($response->ok());
+        }
     }
 
     /**
      * @param string $package
      * @param DependencyUrl $generator
+     * @param bool $sendRequest
      */
-    public function assertTravisUrl(string $package, DependencyUrl $generator)
+    public function assertTravisUrl(string $package, DependencyUrl $generator, bool $sendRequest = true)
     {
         $url = $generator->url();
-        $response = Http::get($url);
 
         $this->assertInstanceOf(DependencyUrl::class, $generator);
         $this->assertStringContainsString($package, $url);
         $this->assertStringContainsString('travis-ci.com', $url);
-        $this->assertTrue($response->ok());
+
+        if ($sendRequest) {
+            $response = Http::get($url);
+
+            $this->assertTrue($response->ok());
+        }
     }
 
     /**
      * @param string $package
      * @param DependencyUrl $generator
+     * @param bool $sendRequest
      */
-    public function assertVersionUrl(string $package, DependencyUrl $generator)
+    public function assertVersionUrl(string $package, DependencyUrl $generator, bool $sendRequest = true)
     {
         $url = $generator->url();
-        $response = Http::get($url);
 
         $this->assertInstanceOf(DependencyUrl::class, $generator);
-        $this->assertTrue($response->ok());
         $this->assertStringContainsString($package, $url);
-        $inString = new StringHelpers($url);
-        $this->assertTrue(
-            $inString->inString('packagist.org/packages') || $inString->inString('hub.docker.com/r/')
-        );
+
+        if ($sendRequest) {
+            $response = Http::get($url);
+
+            $this->assertTrue($response->ok());
+            $inString = new StringHelpers($url);
+            $this->assertTrue(
+                $inString->inString('packagist.org/packages') || $inString->inString('hub.docker.com/r/')
+            );
+        }
     }
 }
